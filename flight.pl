90c1bdb1e32f17ed2a36bcc71edc98b5b639ac32
yanyunbu <iamclaudebu@gmail.com>
2018-11-30 00:02:35 -0800

    update

--- flight.pl
+++ flight.pl
@@ -12,23 +12,19 @@ flight(time(6,0), time(6,1), start3, time(6,2), time(6,3)).
 flight(time(6,0), time(6,1), start4, time(6,2), time(6,3)).
 flight(time(6,0), time(6,1), start5, time(6,2), time(6,3)).
 
-% occupation(F, T, F, J) is the definition of jet bridge occupation
-%                     for F is the fromtime of occupation,
-%                     T is the totime of occupation,
-%                     Flight is flight,
-%                     and J is the number of Jet bridge
+% jetbridge(N, L) is a jetbridge with name N and the list of plane L
+jetbridge(jb1, [flight(time(6,0), time(6,1), start1, time(6,2), time(10,3))]).
+jetbridge(jb2, [flight(time(6,0), time(6,1), start2, time(6,2), time(6,3))]).
+jetbridge(jb3, []).
+jetbridge(jb4, [flight(time(6,0), time(6,1), start4, time(6,2), time(6,3))]).
+jetbridge(jb5, []).
 
-occupation(flight(time(6,0), time(6,1), start1, time(6,2), time(10,3)), j1).
-occupation(flight(time(6,0), time(6,1), start2, time(6,2), time(6,3)), j2).
-occupation(flight(time(6,0), time(6,1), start3, time(6,2), time(6,3)), j3).
-occupation(flight(time(6,0), time(6,1), start4, time(6,2), time(6,3)), j4).
-occupation(flight(time(6,0), time(6,1), start5, time(6,2), time(6,3)), j5).
+% addtobridge(J, F, R) is that add a flight F to one of jet bridge with name J
+%                           and result R
+addtobridge(J, F, R) :-
+    jetbridge(J, L),
+    newflight(L, F, R).
 
-occupation(flight(F1,_,Flight1,_,_), J) :-
-      occupation(flight(_,_,Flight2,_,T), J),
-      dif(Flight1, Flight2),
-      flight(_,_,Flight2,_,T),
-      compare(T,F1).
 
 % compare(A, B) is that time A is earlier than time B
 %
@@ -41,9 +37,11 @@ compare(time(H1,M1), time(H2,M2)) :-
       H1 == H2,
       M1 < M2.
 
-% gettime(T1,T2,R) return time R is later
-gettime(T1, T2, T2) :-
-      compare(T1,T2).
+% newflight(L, F, R) is that add to a jet bridge, L is previous list of jet bridge,
+%                            F is new flight, R is new list
 
-gettime(T1, T2, T1) :-
-      compare(T2,T1).
+newflight([],F,[F]).
+newflight([flight(EA1,AA1,Flight1,ED1,AD1)|T], flight(EA2,AA2,Flight2,ED2,AD2),
+          [flight(EA2,AA2,Flight2,ED2,AD2),flight(EA1,AA1,Flight1,ED1,AD1)|T]) :-
+          compare(AD1,EA2),
+          dif(Flight1,Flight2).
